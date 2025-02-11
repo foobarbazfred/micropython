2283e5eba1a3b9fccc35000ec5a4bc864a35ca4f
Fumio <foobarbazfred@gmail.com>
2025-02-11 15:17:06 +0900

    Update gp2y0a21yk.py

--- sensors/GP2Y0A21YK/src/gp2y0a21yk.py
+++ sensors/GP2Y0A21YK/src/gp2y0a21yk.py
@@ -1,16 +1,23 @@
 #
 # GP2Y0A21YK
+# v0.02 (2025/2/11)
 #
 
 import time
 from machine import ADC, Pin
 
 SENSOR_MEASUREMENT_TIMING = 40  # interval 40msec
-AVAILABLE_LCD = True
+SAMPLIG_SIZE = 21
 
-if AVAILABLE_LCD:
-   import lcd
-   lcd.lcd_cls(lcd.i2c)
+LCD_AVAILABLE = True
+
+adc = ADC(Pin(26))     # 
+
+if LCD_AVAILABLE:
+   from st7032 import ST7032LCD
+   from machine import I2C
+   i2c = I2C(1, scl=Pin(19), sda=Pin(18), freq=10000) # OK??
+   lcd =  ST7032LCD(i2c)
 
 def median_filter(value_list):
     calc_buf  = value_list.copy()
@@ -18,29 +25,28 @@ def median_filter(value_list):
     center_value = calc_buf[int(len(calc_buf)/2)]
     return center_value
 
-value_list = [0] * 21
 
-adc = ADC(Pin(26))     # 
+value_list = [0] * SAMPLIG_SIZE
 
-def measure_loop():
-  while True:
+def measure_distance():
+    global value_list
     # sampling data for fill value_list
-    for _ in range(len(value_list)):
-        gp0.high()
-        adc_val = adc.read_u16()
-        gp0.low()
-        raw_vol = 3.3 * adc_val / 65535
+    for _ in range(SAMPLIG_SIZE):
+        raw_vol = 3.3 * adc.read_u16() / 65535
         value_list.append(raw_vol)
         _ = value_list.pop(0)     # to avoid print out
         time.sleep_us(300)  # wait for avoid power line noise (interval 1ms)
         time.sleep_ms(SENSOR_MEASUREMENT_TIMING)  # wait until sensor output is updated
 
     filterd_value = median_filter(value_list)
-    estim_dist0 = 23.75 / (filterd_value - 0.125) - 0.42   # specification
-    estim_dist1 = 23.36 / (filterd_value - 0.224) - 0.42    # local test
-    estim_dist2 = 1 / (filterd_value * 0.04388 - 0.007506) - 0.42    # local test2
-    print(filterd_value, estim_dist0, estim_dist1, estim_dist2)
+    estim_dist = 28.26 / filterd_value
+    return estim_dist, filterd_value
 
 
-measure_loop()
-    
+while True:
+    dist, vol = measure_distance()
+    report = f'dist: {dist:4.1f}cm\nvol: {vol:4.2f}v'
+    print('----------------------------------')
+    print(report)
+    lcd.print(report,cls=True)
+
