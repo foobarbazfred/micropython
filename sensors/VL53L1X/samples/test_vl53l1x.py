74353fba97fe81813857f1d7dbed36de51c43d2e
Fumio <160304755+foobarbazfred@users.noreply.github.com>
2025-04-26 21:36:08 +0900

    Create test_vl53l1x.py

---
+++ sensors/VL53L1X/samples/test_vl53l1x.py
@@ -0,0 +1,23 @@
+#
+# test program for VL53L1X Driver
+#
+
+from machine import I2C
+from machine import Pin
+import time
+from vl53l1x import VL53L1X
+
+i2c = I2C(0)   # default setting :  scl=Pin(5), sda=Pin(4)
+tof = VL53L1X(i2c)
+
+tof.set_distance_mode('long')
+tof.start_measurement()
+
+while True:
+  if tof.get_data_ready():
+     print(f"dist: {tof.get_distance()} cm")
+     tof.clear_interrupt()
+  else:
+     time.sleep_ms(100)
+
+tof.stop_measurement()
