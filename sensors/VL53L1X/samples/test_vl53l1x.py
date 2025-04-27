d0d79c334e10fc403e585ac867485338b3e50f95
Fumio <160304755+foobarbazfred@users.noreply.github.com>
2025-04-28 08:42:36 +0900

    Update test_vl53l1x.py

--- sensors/VL53L1X/samples/test_vl53l1x.py
+++ sensors/VL53L1X/samples/test_vl53l1x.py
@@ -14,10 +14,13 @@ tof.set_distance_mode('long')
 tof.start_measurement()
 
 while True:
-  if tof.get_data_ready():
+  state = tof.wait_data_ready():
+  if state:
      print(f"dist: {tof.get_distance()} cm")
      tof.clear_interrupt()
   else:
-     time.sleep_ms(100)
+     print("Error: sensor error!!")
+     print("Measurement failed")
+
 
 tof.stop_measurement()
