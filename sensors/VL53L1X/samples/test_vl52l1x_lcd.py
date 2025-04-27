ba60d5f0faca87b0f96dbaa424073d5e3f5d0b32
Fumio <160304755+foobarbazfred@users.noreply.github.com>
2025-04-27 21:27:12 +0900

    Update test_vl52l1x_lcd.py

--- sensors/VL53L1X/samples/test_vl52l1x_lcd.py
+++ sensors/VL53L1X/samples/test_vl52l1x_lcd.py
@@ -9,13 +9,13 @@ from st7032 import ST7032LCD
 
 # i2c setting for Sensor
 # default setting :  SCL:Pin(5), SDA:Pin(4)
-i2c = I2C(0, scl=Pin(5), sda=Pin(4),  freq=10_000) 
-tof = VL53L1X(i2c)
+i2c_0 = I2C(0, scl=Pin(5), sda=Pin(4),  freq=10_000) 
+tof = VL53L1X(i2c_0)
 
 # i2c setting for LCD
 # default setting :  SCL:Pin(19), SDA:Pin(18)
-i2c = I2C(1, scl=Pin(19), sda=Pin(18), freq=10_000)
-lcd = ST7032LCD(i2c)
+i2c_1 = I2C(1, scl=Pin(19), sda=Pin(18), freq=10_000)
+lcd = ST7032LCD(i2c_1)
 
 lcd.cls()  # clear screen
 
