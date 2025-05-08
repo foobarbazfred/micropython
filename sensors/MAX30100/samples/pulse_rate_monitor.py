0aac35342dc3d52b7c0dc9df3a50b1d6fdc07a22
Fumio <160304755+foobarbazfred@users.noreply.github.com>
2025-05-08 23:04:18 +0900

    Update pulse_rate_monitor.py

--- sensors/MAX30100/samples/pulse_rate_monitor.py
+++ sensors/MAX30100/samples/pulse_rate_monitor.py
@@ -6,7 +6,11 @@
 #    moving average N=10 (window size:10)
 #  V0.02 (2025/5/6) refactor; create MAX30100 Driver
 #  V0.03 (2025/5/6) refactor; define constants
-#
+#  V0.04 (2025/5/8) refine algorithm
+#                   not invert graph
+#  V0.04 (2025/5/8) refine algorithm
+#                   change window size 10 -> 5 and change THRESHOLD
+#                  
 #
 
 #
@@ -25,14 +29,11 @@ from max30100 import MAX30100
 
 DEV_ADDR = 0x57
 
-
-VALID_DIFFERENCE_THRESHOLD = 20
+VALID_DIFFERENCE_THRESHOLD = -30
 
 # threshold for Measurement is valid 
 # any data must be larger than this value
 MEASUREMENT_VALIDATION_THRESHOLD = 10000 
-
-
 
 def set_LED_brightness(led, brightness):
     led.duty_u16(brightness)
@@ -43,7 +44,7 @@ def calc_pulse_rate(hr_sensor, led):
    hr_sensor.clear_ovfl_data()
    prev_ir = 0
    n_of_samples=0
-   buffer10 = [0] * 10
+   moving_average = [0] * 5
    ir_buffer100 = [0] * 100
    filterd_data = 0
    prev_tick = time.ticks_us()
@@ -66,22 +67,20 @@ def calc_pulse_rate(hr_sensor, led):
        n_of_samples += 1
 
        # add new data to queue (diffential)
-       buffer10.pop(0)
-       buffer10.append(diff)
+       moving_average.pop(0)
+       moving_average.append(diff)
 
        # add new data to queue (raw data)
        ir_buffer100.pop(0)
        ir_buffer100.append(ir)
 
-       print(ir)
-
        # append moving average filter to diffential(N=10)
        prev_filterd_data = filterd_data
-       filterd_data = int(sum(buffer10)/len(buffer10))
-       filterd_data = filterd_data * -1   #  reverse  wave form
+       filterd_data = int(sum(moving_average)/len(moving_average))
 
        # check cross the threshold
-       if filterd_data > VALID_DIFFERENCE_THRESHOLD and prev_filterd_data <= VALID_DIFFERENCE_THRESHOLD:
+       if filterd_data < VALID_DIFFERENCE_THRESHOLD and prev_filterd_data >= VALID_DIFFERENCE_THRESHOLD  :
+
            # Prevent false detections by ensuring the value is valid.
            min_val = min(ir_buffer100)
            if  min_val > MEASUREMENT_VALIDATION_THRESHOLD:   
@@ -124,15 +123,15 @@ def main():
 
     # feed back led
     led = PWM(Pin(15), freq=500, duty_u16 = 0)
-
-    # setup Heart Rate Sensor
-    i2c0 = I2C(0, scl=Pin(5), sda=Pin(4), freq=100_000)  # 100KHz   
-    if DEV_ADDR in i2c0.scan():
+        
+    i2c = I2C(0, scl=Pin(5),sda=Pin(4),freq=100_000)  # 100KHz
+    
+    if DEV_ADDR in i2c.scan():
         print('connection ok')
     else:
         print('check device connection')
     
-    hr_sensor = MAX30100(i2c0)
+    hr_sensor = MAX30100(i2c)
     hr_sensor.setup()
     
     #
@@ -143,8 +142,6 @@ def main():
 
 main()
 
-
 #
 # end of file
-
 #
