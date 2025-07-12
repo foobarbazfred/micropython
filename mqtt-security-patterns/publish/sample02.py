5abb8c7a4291db20bf3be7b8791b954a085a7e08
Fumio <160304755+foobarbazfred@users.noreply.github.com>
2025-07-12 23:13:32 +0900

    Update sample02.py

--- mqtt-security-patterns/publish/sample02.py
+++ mqtt-security-patterns/publish/sample02.py
@@ -1,3 +1,18 @@
+# sample 02
+# with TLS and no auth
 #
-#
-#
+from umqtt.simple import MQTTClient
+import json
+
+MQTT_BROKER = 'test.mosquitto.org'
+MQTT_PORT = 8883
+MQTT_TOPIC = b'test/upy_publish_test'
+MQTT_CLIENT_ID = "client_RP_Pico2W_0001"
+
+client = MQTTClient(MQTT_CLIENT_ID, MQTT_BROKER, port=MQTT_PORT, ssl=True)
+client.connect()
+message = {'client_id': MQTT_CLIENT_ID , 'security settings' : 'with TLS and no Auth'}
+payload = json.dumps(message).encode('utf-8')
+print("publish:", payload)
+client.publish(MQTT_TOPIC, payload)
+client.disconnect()
