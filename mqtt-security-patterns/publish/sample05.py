ca4a6ecb93d9cf874c9aae6d6a2ab0c75079b1e9
Fumio <160304755+foobarbazfred@users.noreply.github.com>
2025-07-13 21:45:30 +0900

    Create sample05.py

---
+++ mqtt-security-patterns/publish/sample05.py
@@ -0,0 +1,34 @@
+#
+# sample05.py
+#
+from umqtt.simple import MQTTClient
+import ssl
+import json
+
+MQTT_BROKER = 'test.mosquitto.org'
+MQTT_PORT = 8885     # 8885 : MQTT, encrypted, authenticated
+MQTT_TOPIC = b'test/upy_publish_test'
+MQTT_CLIENT_ID = "client_RP_Pico2W_0001"
+MQTT_USER = 'ro'
+MQTT_PASSWORD ='readonly' 
+SERVER_CRT_FILE = 'mosquitto.org.crt.der'
+
+# read Server side ROOT CA (DER format)
+with open(SERVER_CRT_FILE, "rb") as f:
+     cadata = f.read()
+
+# parameters for mTLS
+ssl_params = {
+    'cadata' : cadata,
+    "cert_reqs" : ssl.CERT_REQUIRED,
+    'server_hostname' : MQTT_BROKER,
+}
+client = MQTTClient(MQTT_CLIENT_ID, MQTT_BROKER, MQTT_PORT,
+user = MQTT_USER, password = MQTT_PASSWORD,
+                     ssl = True,  ssl_params = ssl_params )
+client.connect()
+message = {'client_id': MQTT_CLIENT_ID, 
+'connect by' : ' MQTT over TLS, with auth by client CRT'}
+payload = json.dumps(message).encode('utf-8')
+client.publish(MQTT_TOPIC, payload)
+client.disconnect()
