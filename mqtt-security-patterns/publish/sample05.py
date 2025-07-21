514d176cd8e4d0e115190a72e3ab2edd3176ed68
Fumio <160304755+foobarbazfred@users.noreply.github.com>
2025-07-21 14:35:01 +0900

    Update sample05.py

--- mqtt-security-patterns/publish/sample05.py
+++ mqtt-security-patterns/publish/sample05.py
@@ -6,7 +6,8 @@ import ssl
 import json
 
 MQTT_BROKER = 'test.mosquitto.org'
-MQTT_PORT = 8885     # 8885 : MQTT, encrypted, authenticated
+#MQTT_PORT = 8885     # 8885 : MQTT, encrypted, authenticated
+MQTT_PORT = 8887     # 8887 : MQTT, encrypted, authenticated, server certificate deliberately expired
 MQTT_TOPIC = b'test/upy_publish_test'
 MQTT_CLIENT_ID = "client_RP_Pico2W_0001"
 MQTT_USER = 'ro'
