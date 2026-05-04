cda2a21431ee27cc17bff98734fcd7017d2b6cb4
Fumio <160304755+foobarbazfred@users.noreply.github.com>
2026-05-04 22:36:11 +0900

    Update ST7735_test.py

--- displays/ST7735/samples/ST7735_test.py
+++ displays/ST7735/samples/ST7735_test.py
@@ -3,12 +3,7 @@
 #  import mip
 #  mip.install('https://raw.githubusercontent.com/mcauser/micropython-st7735/refs/heads/master/st7735r.py')
 #
-# if you draw text, install terminalfont as followings
-#  import mip
-#  mip.install('https://raw.githubusercontent.com/mcauser/micropython-st7735/refs/heads/master/terminalfont.py')
 #
-#
-
 import time
 from machine import Pin
 from machine import SPI
@@ -34,96 +29,38 @@ dc  = Pin(TFT_DC, Pin.OUT)
 TFT_WIDTH=128
 TFT_HEIGHT=160
 
-COLOR_BLACK = 0x0000
-COLOR_WHITE = 0xFFFF
-COLOR_RED = 0xF800
-COLOR_GREEN = 0x07E0
-COLOR_BLUE = 0x001F
-COLOR_CYAN = 0x07FF
-COLOR_MAGENTA = 0xF81F
-
-
-cyan = 0x07FF
-magenta = 0xF81F
-yellow = 0xFFE0
+# tft.COLOR_BLACK     
+# tft.COLOR_WHITE     
+# tft.COLOR_RED       
+# tft.COLOR_BLUE
+# tft.COLOR_GREEN     
+# tft.COLOR_CYAN      
+# tft.COLOR_MAGENTA   
+# tft.COLOR_YELLOW
 
 spi = SPI(1, baudrate=TFT_SPI_BAUD, polarity=1, phase=1, mosi=mosi, miso=miso, sck=sck )	# blue and green tab work
 tft = ST7735R(spi, dc, cs, rst, w=TFT_WIDTH, h=TFT_HEIGHT, x=0, y=0, rot=0, inv=False, bgr=False)
 tft.init()
-tft.fill(COLOR_BLACK)
-
-#tft.fill(COLOR_WHITE)
-
+tft.fill(tft.COLOR_BLACK)
 
 for _ in range(3):
-    for color in (COLOR_RED, COLOR_GREEN, COLOR_BLUE):
+    for color in (tft.COLOR_RED, tft.COLOR_GREEN, tft.COLOR_BLUE, tft.COLOR_YELLOW):
           tft.fill(color)
-          time.sleep(1)
+          time.sleep(0.5)
 
-tft.fill(COLOR_BLACK)
-tft.rect_outline(0, 0, TFT_WIDTH, TFT_HEIGHT, COLOR_BLUE)
-tft.line(0, 0, TFT_WIDTH, TFT_HEIGHT, COLOR_RED)
-tft.line(TFT_WIDTH, 0, 0, TFT_HEIGHT, COLOR_GREEN)
+tft.fill(tft.COLOR_BLACK)
+tft.rect_outline(0, 0, TFT_WIDTH, TFT_HEIGHT, tft.COLOR_BLUE)
+tft.line(0, 0, TFT_WIDTH, TFT_HEIGHT, tft.COLOR_RED)
+tft.line(TFT_WIDTH, 0, 0, TFT_HEIGHT, tft.COLOR_GREEN)
 
 
 if TFT_WIDTH < TFT_HEIGHT:
    radius = int(TFT_WIDTH/2)
 else:
    radius = int(TFT_HEIGHT/2)
-tft.circle_outline(int(TFT_WIDTH/2),int(TFT_HEIGHT/2),radius,COLOR_CYAN)
-tft.line(10, 10, int(TFT_WIDTH/2), int(TFT_HEIGHT/2), COLOR_RED)
-
-
-#
-# draw text
-#
-import terminalfont
-
-(pos_x, pos_y) = ( int(TFT_WIDTH/4), int(TFT_HEIGHT/5))
-tft.text(pos_x, pos_y, 'Hello,', terminalfont, COLOR_WHITE, size=1)
-tft.text(pos_x + 10, pos_y + 10, 'World!!', terminalfont, COLOR_WHITE, size=1)
+tft.circle_outline(int(TFT_WIDTH/2),int(TFT_HEIGHT/2),radius,tft.COLOR_CYAN)
+tft.line(10, 10, int(TFT_WIDTH/2), int(TFT_HEIGHT/2), tft.COLOR_RED)
 
 #
-# framebuffer test
 #
-import framebuf
-
-# define of frame buffer size
-W = 100  # Width of frame buffer
-H = 30   # Height of frame buffer
-
-# create frame buffer for RGB565, pixel(100 x 30)
-b_ary = bytearray(W * H * 2)
-fbuf = framebuf.FrameBuffer(b_ary, W, H, framebuf.RGB565)
-
-# draw graphics and text into frame buffer
-fbuf.fill(0)
-fbuf.rect(0,0,W,H,0x00ff,False)
-fbuf.text('MicroPython!', 2, 10 , 0xff00)
-
-# define draing position in LCD
-(POS_X , POS_Y) = (10, 50)
-
-# show contents of framebuffer to LCD
-tft._set_window(POS_X, POS_Y, POS_X + W - 1,POS_Y + H - 1)
-tft.data(b_ary)
-
-
-
-
-
 #
-# MEMO
-#
-
-
-#https://raw.githubusercontent.com/mcauser/micropython-st7735/refs/heads/master/st7735r.py
-#https://github.com/mcauser/micropython-st7735/tree/master
-
-# GP8  SPI1 Rx
-# GP9  SPI1 CSn
-# GP10 SPI1 SCK
-# GP11 SPI1 TX
-# GP12 A0 
-# GP13 CS
-# GP14 RESET
