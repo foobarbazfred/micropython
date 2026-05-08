a9776368d93d6cfa769afbc93b1609bde1337557
Fumio <160304755+foobarbazfred@users.noreply.github.com>
2026-05-09 01:11:06 +0900

    Update blink_w_set.py

--- rp2350_pio/samples/blink_w_set.py
+++ rp2350_pio/samples/blink_w_set.py
@@ -1,5 +1,6 @@
 import time
 import rp2
+import machine
 from machine import Pin
 
 GP_SET_PIN=1
@@ -11,6 +12,10 @@ def blink_w_set():
     set(pins, 1)
     set(pins, 0)
     # wrap()           # may skip
+
+
+# to enable 2KHz to StateMachine, set systemclock to 125MHz
+machine.freq(125_000_000)
 
 sm = rp2.StateMachine(STATE_MACHINE_ID, blink_w_set, freq=2000, set_base=Pin(GP_SET_PIN))
 
