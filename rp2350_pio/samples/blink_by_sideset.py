2caf7847b9563f0a8ae6ac1e6bc9285b908266a4
Fumio <160304755+foobarbazfred@users.noreply.github.com>
2026-05-09 01:11:37 +0900

    Update blink_by_sideset.py

--- rp2350_pio/samples/blink_by_sideset.py
+++ rp2350_pio/samples/blink_by_sideset.py
@@ -1,5 +1,6 @@
 import time
 import rp2
+import machine
 from machine import Pin
 
 GP_SIDESET_PIN=1
@@ -11,6 +12,9 @@ def blink_w_sideset():
     nop() .side(0)
     nop() .side(1)
     # wrap()           # may skip
+
+# to enable 2KHz to StateMachine, set systemclock to 125MHz
+machine.freq(125_000_000)
 
 sm = rp2.StateMachine(STATE_MACHINE_ID, blink_w_sideset, freq=2000, side_base=Pin(GP_SIDESET_PIN))
 
