9d239a31ca6bbf1b0468eaf9bddc740d8cd9fa03
Fumio <160304755+foobarbazfred@users.noreply.github.com>
2026-05-08 23:23:38 +0900

    Update blink_by_sideset.py

--- rp2350_pio/samples/blink_by_sideset.py
+++ rp2350_pio/samples/blink_by_sideset.py
@@ -7,8 +7,10 @@ STATE_MACHINE_ID=0
 
 @rp2.asm_pio(sideset_init=rp2.PIO.OUT_LOW)
 def blink_w_sideset():
+    # wrap_target()    #  may skip
     nop() .side(0)
     nop() .side(1)
+    # wrap()           # may skip
 
 sm = rp2.StateMachine(STATE_MACHINE_ID, blink_w_sideset, freq=2000, side_base=Pin(GP_SIDESET_PIN))
 
