179f79e7713e1278e7dd6e4dc24a7b4d54ba707f
Fumio <foobarbazfred@gmail.com>
2026-05-10 19:31:58 +0900

    Create blink_w_out.py

---
+++ rp2350_pio/samples/blink_w_out.py
@@ -0,0 +1,26 @@
+import time
+import rp2
+import machine
+from machine import Pin
+
+GP_OUT_PIN = 1
+STATE_MACHINE_ID = 0
+
+@rp2.asm_pio(out_init=rp2.PIO.OUT_LOW, out_shiftdir=rp2.PIO.SHIFT_RIGHT)
+def blink_w_out():
+    # wrap_target()    # may skip
+    pull()             # TX FIFO -> OSR
+    out(pins, 1)       # OSR -> GPIO  with  1bit shift
+    # wrap()           # may skip
+
+
+# to enable 2KHz to StateMachine, set systemclock to 125MHz
+machine.freq(125_000_000)
+
+sm = rp2.StateMachine(STATE_MACHINE_ID, blink_w_out, freq=2000, out_base=Pin(GP_OUT_PIN))
+
+sm.active(1)
+while True:
+    sm.put(1)  # set 1 to TX FIFO
+    sm.put(0)  # set 0 to TX FIFO
+sm.active(0)
