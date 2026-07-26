95355d1b7f0dac5bf39ec08e1598d2d4a7a4b011
Fumio <foobarbazfred@gmail.com>
2026-07-26 20:23:18 +0900

    DVI generate test w/HSTX

---
+++ rp2350-hstx/src/dvi-test.py
@@ -0,0 +1,361 @@
+#
+# Test program for DVI signal generation (V0.03)  2026/7/26
+#
+#  (1) Adjusted system clock to 126 MHz
+#  (2) Generates HSYNC/VSYNC and fills the display area with a solid color
+#  (3) Uses two DMA channels that trigger each other
+#  (4) Resets the start address in software via IRQ
+#
+
+
+import time
+import machine
+from rp2 import DMA
+import array
+
+from mylib import write_reg
+from mylib import write_CLK_HSTX_CTRL
+from mylib import write_CLK_HSTX_DIV
+from mylib import write_HSTX_CTRL_CSR
+from mylib import write_HSTX_CTRL_BIT
+from mylib import write_HSTX_CTRL_EXPAND_TMDS
+from mylib import write_HSTX_CTRL_EXPAND_SHIFT
+
+
+C1C0_BLANK = 0x354
+C1C0_HSYNC = 0x0AB
+C1C0_VSYNC = 0x154
+C1C0_HVSYNC = 0x2AB
+
+#
+# connection of DVI Socket Board
+# https://github.com/wren6991/pico-dvi-sock
+#
+# bit0 /  D0+  / GP12   Blue
+# bit1 /  D0-  / GP13   Blue
+#
+# bit2 /  CLK+ / GP14   CLK
+# bit3 /  CLK- / GP15   CLK
+#
+# bit4 /  D2+  / GP16   Red 
+# bit5 /  D2-  / GP17   Red
+#
+# bit6 /  D1+  / GP18   Green
+# bit7 /  D1-  / GP19   Green
+
+
+# Front PORCH
+#   (BLANK*16 + HSYNC*96 + BLANK*48 + BLANK*640) * 10
+#
+# VSYNC
+#   (VSYNC*16 + HHSYNC*96 + VSYNC*48 + VSYNC*640) * 2
+#
+# Back PORCH
+#   (BLANK*16 + HSYNC*96 + BLANK*48 + BLANK*640) * 33
+#
+# Display Lines
+#  (BLANK*16 + HSYNC*96 + BLANK*48 + disp_area*640) * 480
+
+
+# 32bit pack (R(10bit):G(10bit):B(10bit))
+PACK_BLANK = C1C0_BLANK << 20 | C1C0_BLANK << 10 | C1C0_BLANK
+PACK_HSYNC = C1C0_BLANK << 20 | C1C0_BLANK << 10 | C1C0_HSYNC
+PACK_VSYNC = C1C0_BLANK << 20 | C1C0_BLANK << 10 | C1C0_VSYNC
+PACK_HVSYNC = C1C0_BLANK << 20 | C1C0_BLANK << 10 | C1C0_HVSYNC
+
+
+#
+# set system clock 126MHz
+#
+
+machine.freq(126_000_000)
+
+
+
+#
+# setup GPIO (GP12-GP19)
+#
+
+# select function as HSTX
+# 9.11.1. IO - User Bank P592
+BASE_IO_BANK0 = 0x40028000
+set_val = 0x0  # select HSTX
+for gp_no in range(12,20):
+    write_reg(BASE_IO_BANK0, gp_no * 8 + 4, set_val)
+
+# setup PAD
+# 9.11.3. Pad Control - User Bank P771
+BASE_PADS_BANK0 = 0x40038000
+set_val = 0x10  # ISO:0, IE:0, DRIVE:0x1(4mA), PUE:0, PDE:0,
+for gp_no in range(12,20):
+    write_reg(BASE_PADS_BANK0, gp_no * 4 + 4, set_val)
+
+
+#
+#
+# Clock Setup
+#
+#
+
+CLK_ENABLE = 1
+CLK_SYS = 0
+
+# SYS_CLK
+CLK_ENABLE = 1
+CLK_SYS = 0
+val = (CLK_ENABLE << 11) + (CLK_SYS << 5)
+write_CLK_HSTX_CTRL(0)  # disable clock
+div = 1 << 16
+write_CLK_HSTX_DIV(div)   # 150 MHz
+write_CLK_HSTX_CTRL(val)  # enable and PLL source
+
+
+#
+# HSTX SETUP
+#
+
+#
+# EXPAND SHIFT
+#
+# EXPAND_SHIFT
+# RAW_SHIFT: 32
+# RAW_N_SHIFTS: 1
+#RAW_SHIFT = 32
+RAW_SHIFT = 0       # 0 means 32
+RAW_N_SHIFTS = 1
+
+# encoded expand
+# each 16bit (RGB:565) and 2times
+#
+ENC_SHIFT=16
+ENC_N_SHIFTS=2
+#
+#
+val = ENC_N_SHIFTS << 24 | ENC_SHIFT << 16 |RAW_N_SHIFTS << 8 | RAW_SHIFT
+write_HSTX_CTRL_EXPAND_SHIFT(val)
+
+
+#
+# EXPAND TMDS  RGB:565
+# L0_ROT = 32 - (7 - 4)   # to shift left 3 (bit 7 - bit 4) , equals to shift right 32 - 3
+# L0_NBITS = 4  (5-1)
+# L1_ROT = 5
+# L1_NBITS = 5  (6-1)
+# L2_ROT = 11
+# L2_NBITS = 4  (5-1)
+#
+l2_nbits = 5 - 1
+l2_rot =  (5 + 6 + 5 - 1) - 7
+l1_nbits = 6 - 1
+l1_rot = (5 + 6 - 1) - 7
+l0_nbits = 5 - 1
+l0_rot = 32 - (7 - 4)
+
+val = l2_nbits << 21 | l2_rot << 16 | l1_nbits << 13 |  l1_rot << 8 |  l0_nbits << 5 |  l0_rot 
+write_HSTX_CTRL_EXPAND_TMDS(val)
+
+
+#
+# setup BIT
+#
+
+
+INV_FLG = 1
+OFFSET_INV_BIT = 16
+CLK_FLG = 1
+OFFSET_CLK_BIT = 17
+
+# BIT0
+nth_bit = 0
+sel_p = 0
+sel_n = sel_p + 1
+val = sel_n << 8 | sel_p
+write_HSTX_CTRL_BIT(nth_bit, val)
+
+# BIT1
+nth_bit = 1
+val |= INV_FLG << OFFSET_INV_BIT
+write_HSTX_CTRL_BIT(nth_bit, val)
+
+# BIT2
+nth_bit = 2
+val = CLK_FLG << OFFSET_CLK_BIT
+write_HSTX_CTRL_BIT(nth_bit, val)
+
+# BIT3
+nth_bit = 3
+val |= INV_FLG << OFFSET_INV_BIT
+write_HSTX_CTRL_BIT(nth_bit, val)
+
+# BIT4
+nth_bit = 4
+sel_p = 20
+sel_n = sel_p + 1
+val = sel_n << 8 | sel_p
+write_HSTX_CTRL_BIT(nth_bit, val)
+
+# BIT5
+nth_bit = 5
+val |= INV_FLG << OFFSET_INV_BIT
+write_HSTX_CTRL_BIT(nth_bit, val)
+
+# BIT6
+nth_bit = 6
+sel_p = 10
+sel_n = sel_p + 1
+val = sel_n << 8 | sel_p
+write_HSTX_CTRL_BIT(nth_bit, val)
+
+# BIT7
+nth_bit = 7
+val |= INV_FLG << OFFSET_INV_BIT
+write_HSTX_CTRL_BIT(nth_bit, val)
+
+
+#
+#
+#
+
+CLK_DIV=5
+N_SHIFTS = 5
+SHIFT = 2
+val =  CLK_DIV << 28 | N_SHIFTS << 16 | SHIFT << 8 | 0x3
+write_HSTX_CTRL_CSR(val)
+
+
+#---------------------------------------------------------------------------------------
+#  setup done
+
+
+
+#
+# create VGA Buffer
+#
+
+VGA_WIDTH=640
+VGA_HEIGHT=480
+
+def init_vga_data():
+
+    data_size = 6 * 10 +  6 * 2 + 6 * 33 + 8 * VGA_HEIGHT
+
+    buffer = array.array('I', [0] * data_size)  # I means unsigned int
+    index = 0
+
+    # setup pre  BLANK * n + HSYNC * n + BLANK * n + BLANK * VGA_WIDTH
+    for _ in range(10):
+        buffer[index] = 0x00_00_10_00 + 16
+        index += 1
+        buffer[index] = PACK_BLANK
+        index += 1
+        buffer[index] = 0x00_00_10_00 + 96
+        index += 1
+        buffer[index] = PACK_HSYNC
+        index += 1
+        buffer[index] = 0x00_00_10_00 + (48 + VGA_WIDTH)
+        index += 1
+        buffer[index] = PACK_BLANK
+        index += 1
+
+    # setup pre  BLANK * n + HSYNC * n + BLANK * n + BLANK * VGA_WIDTH
+    for _ in range(2):
+        buffer[index] = 0x00_00_10_00 + 16
+        index += 1
+        buffer[index] = PACK_VSYNC
+        index += 1
+        buffer[index] = 0x00_00_10_00 + 96
+        index += 1
+        buffer[index] = PACK_HVSYNC
+        index += 1
+        buffer[index] = 0x00_00_10_00 + (48 + VGA_WIDTH)
+        index += 1
+        buffer[index] = PACK_VSYNC
+        index += 1
+
+    # setup pre  BLANK * n + HSYNC * n + BLANK * n + BLANK * VGA_WIDTH
+    for _ in range(33):
+        buffer[index] = 0x00_00_10_00 + 16
+        index += 1
+        buffer[index] = PACK_BLANK
+        index += 1
+        buffer[index] = 0x00_00_10_00 + 96
+        index += 1
+        buffer[index] = PACK_HSYNC
+        index += 1
+        buffer[index] = 0x00_00_10_00 + (48 + VGA_WIDTH)
+        index += 1
+        buffer[index] = PACK_BLANK
+        index += 1
+
+    #
+    # image area (640x480)
+    # setup pre  BLANK * n + HSYNC * n + BLANK * n + BLANK * VGA_WIDTH
+    for _ in range(VGA_HEIGHT):
+        buffer[index] = 0x00_00_10_00 + 16
+        index += 1
+        buffer[index] = PACK_BLANK
+        index += 1
+        buffer[index] = 0x00_00_10_00 + 96
+        index += 1
+        buffer[index] = PACK_HSYNC
+        index += 1
+        buffer[index] = 0x00_00_10_00 + 48
+        index += 1
+        buffer[index] = PACK_BLANK
+        index += 1
+
+        buffer[index] = 0x00_00_30_00 + VGA_WIDTH
+        index += 1
+        buffer[index] = 0b00_0100100101_0011010010_1010010100
+        index += 1
+
+    return buffer
+
+#
+# create buffer for VGA display
+#
+vga_buffer = init_vga_data()
+buffer_size = len(vga_buffer)
+
+#
+# setup DMA
+#
+#  dreq:  DREQ_HSTX(52)
+#
+
+dma0 = DMA()
+dma1 = DMA()
+
+def dma0_itr_handler(arg):
+    dma0.read = vga_buffer
+    #print('.',end='')
+
+def dma1_itr_handler(arg):
+    dma1.read = vga_buffer
+
+def setup_dma(dma0,dma1,buffer, buffer_size):
+
+    BASE_HTSX_FIFO = 0x5060_0000
+    HSTX_FIFO_ADDR = BASE_HTSX_FIFO + 0x4
+    
+    dma0.irq(handler=dma0_itr_handler, hard=True)
+    dma1.irq(handler=dma1_itr_handler, hard=True)
+
+    dma0_ctrl = dma0.pack_ctrl(size=2, inc_read=True, inc_write=False, treq_sel=52, irq_quiet=False, chain_to=dma1.channel)
+    dma0.config(read=buffer, write=HSTX_FIFO_ADDR, ctrl=dma0_ctrl, trigger=False, count=buffer_size)
+
+    dma1_ctrl = dma1.pack_ctrl(size=2, inc_read=True, inc_write=False, treq_sel=52, irq_quiet=False, chain_to=dma0.channel)
+    dma1.config(read=buffer, write=HSTX_FIFO_ADDR, ctrl=dma1_ctrl, trigger=False, count=buffer_size)
+                                                                                           
+
+setup_dma(dma0, dma1, vga_buffer, buffer_size)
+
+
+# start to generate DVI signal
+dma0.active(True)
+
+
+
+#
+#
+#
---
+++ rp2350-hstx/src/mylib.py
@@ -0,0 +1,216 @@
+#
+# helper functions for r/w registers
+#
+
+
+import machine
+
+BASE_USER_BANK_IO = 0x40028000
+OFFSET_GPIO16_STATUS = 0x080
+OFFSET_GPIO16_CTRL = 0x084
+
+# PADS_BANK0: GPIO16 Register
+
+
+BASE_PADS_BANK0 = 0x40038000
+# (defined as PADS_BANK0_BASE in SDK).
+OFFSET_PADS_BANK0_GPIO16 = 0x44  # bits 8: ISO
+
+
+def write_reg(base, offset, value):
+    machine.mem32[base + offset] = value
+
+def read_reg(base, offset):
+    return machine.mem32[base + offset]
+
+def read_GPIO16_STATUS_register():
+    return read_reg(BASE_USER_BANK_IO, OFFSET_GPIO16_STATUS)
+
+def read_GPIO16_CTRL_register():
+    return read_reg(BASE_USER_BANK_IO, OFFSET_GPIO16_CTRL)
+
+def write_GPIO16_CTRL_register(value):
+    return write_reg(BASE_USER_BANK_IO, OFFSET_GPIO16_CTRL, value)
+
+def read_PADS_BANK0_GPIO_register(gp_no):
+    return read_reg(BASE_PADS_BANK0, 4*gp_no + 0x04)
+
+
+# 0x40028000 (defined as IO_BANK0_BASE in SDK).
+
+def write_GPIO_CTRL_register(gp_no, value):
+    base = BASE_USER_BANK_IO
+    offset = 8 * gp_no + 0x004
+    return write_reg(base, offset, value)
+
+def read_GPIO_CTRL_register(gp_no):
+    base = BASE_USER_BANK_IO
+    offset = 8 * gp_no + 0x004
+    return read_reg(base, offset)
+
+
+BASE_PADS_BANK0 = 0x40038000   # P771
+ISO_OFFSET = 8
+def clr_PADS_BANK0_GPIO_ISO(gp_no):
+    base = BASE_PADS_BANK0
+    offset = 4 * gp_no + 0x04
+    reg_val = read_PADS_BANK0_GPIO(gp_no)
+    reg_val = (~ (1 << ISO_OFFSET) & 0xFFFF_FFFF) & reg_val
+    write_reg(base,offset,reg_val)
+
+def read_PADS_BANK0_GPIO(gp_no):
+    base = BASE_PADS_BANK0
+    offset = 4 * gp_no + 0x04
+    reg_val = read_reg(base,offset)
+    return reg_val  
+
+def write_PADS_BANK0_GPIO(gp_no,val):
+    base = BASE_PADS_BANK0
+    offset = 4 * gp_no + 0x04
+    reg_val = write_reg(base,offset,val)
+    return reg_val  
+
+
+#
+# HSTX_CTRL_BIT_N
+#
+BASE_HTSX_CTRL = 0x400c_0000
+
+def write_HSTX_CTRL_BIT(nth_bit, val):
+    base = BASE_HTSX_CTRL
+    offset = 4 * (nth_bit + 1)
+    write_reg(base, offset, val)
+
+def read_HSTX_CTRL_BIT(nth_bit):
+    base = BASE_HTSX_CTRL
+    offset = 4 * (nth_bit + 1)
+    return read_reg(base, offset)
+
+
+#
+# HSTX_CTRL_CSR
+#
+BASE_HTSX_BTRL = 0x400c_0000
+
+def write_HSTX_CTRL_CSR(value):
+    base = BASE_HTSX_CTRL
+    offset = 0
+    write_reg(base, offset, value)
+
+def read_HSTX_CTRL_CSR():
+    base = BASE_HTSX_CTRL
+    offset = 0
+    return read_reg(base, offset)
+
+
+def set_HSTX_CTRL_CSR_EN():
+    val = read_HSTX_CTRL_CSR()
+    val |= 1
+    write_HSTX_CTRL_CSR(val)
+
+BASE_HTSX_FIFO = 0x5060_0000
+def write_HSTX_FIFO(value):
+    base = BASE_HTSX_FIFO
+    offset = 0x4
+    write_reg(base, offset, value)
+    
+
+def read_HSTX_FIFO_stat():
+    base = BASE_HTSX_FIFO
+    offset = 0
+    return read_reg(base, offset)
+
+
+#0x400c0000 (defined as HSTX_CTRL_BASE in
+OFFSET_EXPAND_SHIFT=0x24
+def write_HSTX_CTRL_EXPAND_SHIFT(val):
+    base = BASE_HTSX_CTRL
+    offset = OFFSET_EXPAND_SHIFT
+    return write_reg(base, offset,val)
+    
+def read_HSTX_CTRL_EXPAND_SHIFT():
+    base =  BASE_HTSX_CTRL
+    offset = OFFSET_EXPAND_SHIFT
+    return read_reg(base, offset)
+    
+
+OFFSET_EXPAND_TMDS=0x28
+def write_HSTX_CTRL_EXPAND_TMDS(val):
+    base = BASE_HTSX_CTRL
+    offset = OFFSET_EXPAND_TMDS
+    return write_reg(base, offset,val)
+
+def read_HSTX_CTRL_EXPAND_TMDS():
+    base =  BASE_HTSX_CTRL
+    offset = OFFSET_EXPAND_TMDS
+    return read_reg(base, offset)
+    
+
+BASE_CLOCKS = 0x40010000 
+OFFSET_CLK_HSTX_CTRL = 0x54
+OFFSET_CLK_HSTX_DIV = 0x58
+
+
+def read_CLK_HSTX_CTRL():
+    base = BASE_CLOCKS
+    offset = OFFSET_CLK_HSTX_CTRL
+    return read_reg(base, offset)
+    
+def write_CLK_HSTX_CTRL(val):
+    base = BASE_CLOCKS
+    offset = OFFSET_CLK_HSTX_CTRL
+    return write_reg(base, offset,val)
+    
+def read_CLK_HSTX_DIV():
+    base = BASE_CLOCKS
+    offset = OFFSET_CLK_HSTX_DIV
+    return read_reg(base, offset)
+    
+def write_CLK_HSTX_DIV(val):
+    base = BASE_CLOCKS
+    offset = OFFSET_CLK_HSTX_DIV
+    return write_reg(base, offset,val)
+    
+
+
+
+
+
+# >>> hex(read_CLK_HSTX_DIV())
+# '0x10000'
+# >>> 160 * 1000 / 65536
+# 2.4414062
+# 2KHz
+
+BASE_RESETS = 0x40020000
+OFFSET_RESETS_RESET = 0
+def read_RESETS_RESET():
+    base = BASE_CLOCKS
+    offset = OFFSET_RESETS_RESET
+    return read_reg(base, offset)
+
+
+
+
+
+#>>> hex(read_CLK_HSTX_CTRL())
+#'0x10_00_08_00'
+# '0b  0001_0000_0000_0000_0000_1000_0000_0000
+# ENABLE:1 , ENABLED:1
+
+
+###
+#
+
+# >>> hex(read_CLK_HSTX_DIV())
+# '0x10000'   65536 
+# clock : 2KHz ???
+
+
+#address of 0x40028000 (defined as IO_BANK0_BASE in SDK).
+# 0x09c GPIO19_CTRL
+
+
+# GPIO19_CTRL
+#hex(read_reg(0x40028000,0x9c))
+
